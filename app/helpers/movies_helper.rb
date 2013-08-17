6f15e182b8940a9f5f82a142de08ab1f2993d43e
robertp82 <robwp21@gmail.com>
2013-08-17 11:04:36 -0400

    Move helper methods to helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,39 @@
-module MoviesHelper
+module MoviesHelper  
+  
+  #This was used to seed the database with dumps of file trees of existing movies
+  #You could modify the movie parsing and use it. It's reading in a "Movie Title (YEAR).ext" format.
+  def self.add_movies file_path, watched
+    File.open(file_path, "r").each_line do |line|
+      title = line[0...-13]
+      movie = Movie.new()
+      movie.name = title
+      movie.update_by_name
+      movie.watched = watched
+      begin
+        movie.save
+      rescue
+        puts "fail" + line
+      end
+    end
+  end   
+  
+  
+  #This method is used to refresh data from themoviedb
+  def self.update_all_movies
+    movies = Movie.all
+    movies.each do | m |
+      if m.runtime.nil?
+        mov = TmdbMovie.find(:id => m.tmdb_id)
+        m.update_by_tmdb_obj mov     
+        #m.update_by_name      
+        #m.watched = true
+        begin
+          m.save
+        rescue
+          put "failed to save"
+        end
+      end
+    end
+  end
+  
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -54,37 +54,4 @@ class Movie < ActiveRecord::Base
     #self.watched = false    
   end
   
-  
-  
-  def self.add_movies file_path, watched
-    File.open(file_path, "r").each_line do |line|
-      title = line[0...-13]
-      movie = Movie.new()
-      movie.name = title
-      movie.update_by_name
-      movie.watched = watched
-      begin
-        movie.save
-      rescue
-        puts "fail" + line
-      end
-    end
-  end   
-  
-  def self.update_all_movies
-    movies = Movie.all
-    movies.each do | m |
-      if m.runtime.nil?
-        mov = TmdbMovie.find(:id => m.tmdb_id)
-        m.update_by_tmdb_obj mov     
-        #m.update_by_name      
-        #m.watched = true
-        begin
-          m.save
-        rescue
-          put "failed to save"
-        end
-      end
-    end
-  end
 end
